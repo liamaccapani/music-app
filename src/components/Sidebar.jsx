1c946721c064d270dc6e379bd0b1b5eed1aaec56
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-15 18:54:51 +0100

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,14 @@ import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div class="sidebar">
-      <img src="./spotify-logo.png" alt="spotify logo" />
+      <div className="img_container">
+        <img src="./spotify-logo.png" alt="spotify logo" />
+      </div>
       <div id="menu">
         <div class="nav-item">1</div>
-        <div class="nav-item">2</div>
+        {/* <div class="nav-item">2</div>
         <div class="nav-item">3</div>
-        <div class="nav-item">4</div>
+        <div class="nav-item">4</div> */}
       </div>
     </div>
   );
