698299a6d4f7e44e85da827c1f1a8a373477aec4
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-17 12:23:07 +0100

    styles of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,45 @@
 import "./Sidebar.css";
-import Grid from "@mui/material/Grid";
 import { MdHomeFilled } from "react-icons/md";
 import { FiSearch } from "react-icons/fi";
 import { BiLibrary } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    // <Grid item md={3} className="sidebar">
-    //   <div id="menu">
-    //     <div class="nav-item">1</div>
-    //     <div class="nav-item">2</div>
-    //     <div class="nav-item">3</div>
-    //     <div class="nav-item">4</div>
-    //   </div>
-    // </Grid>
-    <div className="sidebar">
-      <div id="menu">
+    <div id="sidebar">
+      <div className="menu one">
+        <div className="nav-item">
+          <MdHomeFilled />
+          <span>Home</span>
+        </div>
+        <div className="nav-item">
+          <FiSearch />
+          <span>Search</span>
+        </div>
+        <div className="nav-item">
+          <BiLibrary />
+          <span>Your Library</span>
+        </div>
+      </div>
+
+      <div className="menu two">
         <div class="nav-item">1</div>
+        <div class="nav-item">2</div>
+        <div class="nav-item">3</div>
+        <div class="nav-item">4</div>
+      </div>
+
+      <div className="menu three">
+        <div class="nav-item">1</div>
+        <div class="nav-item">2</div>
+        <div class="nav-item">3</div>
+        <div class="nav-item">4</div>
+        <div class="nav-item">1</div>
+        <div class="nav-item">2</div>
+        <div class="nav-item">3</div>
+        <div class="nav-item">4</div>
+        <div class="nav-item">2</div>
+        <div class="nav-item">3</div>
+        <div class="nav-item">4</div>
         <div class="nav-item">2</div>
         <div class="nav-item">3</div>
         <div class="nav-item">4</div>
@@ -27,11 +50,11 @@ const Sidebar = () => {
 
 export default Sidebar;
 
-// <div class="sidebar">
+// <Grid item md={3} className="sidebar">
 //   <div id="menu">
 //     <div class="nav-item">1</div>
-//     {/* <div class="nav-item">2</div>
+//     <div class="nav-item">2</div>
 //     <div class="nav-item">3</div>
-//     <div class="nav-item">4</div> */}
+//     <div class="nav-item">4</div>
 //   </div>
-// </div>
+// </Grid>
