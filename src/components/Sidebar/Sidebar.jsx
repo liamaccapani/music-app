15c254239640949ab16b168656b336fce32d694a
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-17 11:43:05 +0100

    Homepage layout (sidebar + player)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,37 @@
 import "./Sidebar.css";
+import Grid from "@mui/material/Grid";
 import { MdHomeFilled } from "react-icons/md";
-import { FiSearch } from "react-icons/fi"; 
-import { BiLibrary } from "react-icons/bi"; 
+import { FiSearch } from "react-icons/fi";
+import { BiLibrary } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div class="sidebar">
+    // <Grid item md={3} className="sidebar">
+    //   <div id="menu">
+    //     <div class="nav-item">1</div>
+    //     <div class="nav-item">2</div>
+    //     <div class="nav-item">3</div>
+    //     <div class="nav-item">4</div>
+    //   </div>
+    // </Grid>
+    <div className="sidebar">
       <div id="menu">
         <div class="nav-item">1</div>
-        {/* <div class="nav-item">2</div>
+        <div class="nav-item">2</div>
         <div class="nav-item">3</div>
-        <div class="nav-item">4</div> */}
+        <div class="nav-item">4</div>
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+
+// <div class="sidebar">
+//   <div id="menu">
+//     <div class="nav-item">1</div>
+//     {/* <div class="nav-item">2</div>
+//     <div class="nav-item">3</div>
+//     <div class="nav-item">4</div> */}
+//   </div>
+// </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
+import "./Home.css";
+import Grid from "@mui/material/Grid";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Player from "../components/Player/Player";
 
 const Home = () => {
-    return (
-      <>
-       <Sidebar/>
-       {/* <div>HOME</div> */}
-       <Player/>
-      </>
-    )
-  };
-  
-  export default Home;
+  return (
+    <>
+      <Grid container className="whole-page">
+        <Grid item md={2}>
+          <Sidebar />
+        </Grid>
+        <Grid item md={10} className="home-page">
+          <div>HOME</div>
+        </Grid>
+      </Grid>
+      <Player />
+    </>
+  );
+};
+
+export default Home;
